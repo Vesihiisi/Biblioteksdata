e329462550c7ea8ca422a8d08b1ebc1f2d5fc335
Vesihiisi <alicia@fagerving.se>
2018-08-07 14:17:32 +0200

    cleanup

--- importer/process_refs.py
+++ importer/process_refs.py
@@ -11,11 +11,17 @@ FILENAME = "isbn_sorted_medium.tsv"
 
 
 def only_digits(input):
-    """Some ISBN nos in Libris have ;"""
+    """
+    Remove punctuation from ISBN.
+
+    Some ISBN numbers in Libris have ;
+    in them.
+    """
     return re.sub('[^\w\s]', '', input)
 
 
 def load_frequencies(fname):
+    """Load the ISBN frequency file."""
     frequencies = {}
     with open(fname) as tsvfile:
         reader = csv.reader(tsvfile, delimiter='\t')
@@ -28,19 +34,13 @@ def load_frequencies(fname):
     return frequencies
 
 
-def is_match(idno, record):
-    isbn_in_libris = record["isbn"]
-    if isinstance(isbn_in_libris, str):
-        if only_digits(isbn_in_libris) == idno:
-            return True
-    elif isinstance(isbn_in_libris, list):
-        for potential_isbn in isbn_in_libris:
-            if only_digits(potential_isbn) == idno:
-                return True
-    return False
-
-
 def download_data(isbn):
+    """
+    Download data about an ISBN.
+
+    Search for an ISBN in the API
+    and return all the matching records.
+    """
     book_url = URL.format(isbn)
     book_data = json.loads(requests.get(book_url).text)
     records = book_data["xsearch"]["records"]
@@ -59,27 +59,11 @@ if __name__ == "__main__":
     frequencies = load_frequencies(args.path)
     for item in frequencies:
         records = download_data(item)
-        if records:
+        if records and len(records) == 1:
             for record in records:
-                if is_match(item, record):
-                    works[item] = {}
-                    works[item]["count"] = frequencies[item]["count"]
-                    works[item]["isbn"] = item
-                    if "creator" in record.keys():
-                        creator = record["creator"]
-                        count = frequencies[item]["count"]
-                        works[item]["author"] = creator
-                        if creator not in authors.keys():
-                            authors[creator] = int(count)
-                        else:
-                            authors[creator] = authors[creator] + int(count)
-                    else:
-                        works[item]["author"] = ""
-                    works[item]["title"] = record["title"]
-                    works[item]["libris"] = record["identifier"].split(
-                        "/")[-1]
-                    works[item]["language"] = record["language"]
-                    print(works[item])
+                works[item] = record
+                works[item]["count"] = frequencies[item]["count"]
+                works[item]["isbn"] = item
 
     with open('output.tsv', 'w') as output_file:
         csvwriter = csv.writer(output_file, delimiter='\t')
@@ -87,10 +71,4 @@ if __name__ == "__main__":
                             "title", "LIBRIS", "language"])
         for item in works:
             csvwriter.writerow(works[item].values())
-        output_file.close()
-    with open('output_authors.tsv', 'w') as output_file:
-        csvwriter = csv.writer(output_file, delimiter='\t')
-        csvwriter.writerow(["author", "count"])
-        for item in authors:
-            csvwriter.writerow([item, authors[item]])
         output_file.close()
