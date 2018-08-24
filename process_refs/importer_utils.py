64f22e9d71707a3712d2e05e1111376e0eac3c0e
Alicia Fagerving <alicia@fagerving.se>
2018-08-24 15:03:33 +0200

    Separate ref and auth processing (#11)
    Put the code for reference processing and auth uploads in separate sections.
    
    Task: https://phabricator.wikimedia.org/T202728

---
+++ process_refs/importer_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+import wikidataStuff.wdqsLookup as lookup
+
+
+def get_wd_items_using_prop(prop):
+    """
+    Get WD items that already have some value of a unique ID.
+
+    Even if there are none before we start working,
+    it's still useful to have in case an upload is interrupted
+    and has to be restarted, or if we later want to enhance
+    some items. When matching, these should take predecence
+    over any hardcoded matching files.
+
+    The output is a dictionary of ID's and items
+    that looks like this:
+    {'4420': 'Q28936211', '2041': 'Q28933898'}
+    """
+    items = {}
+    print("WILL NOW DOWNLOAD WD ITEMS THAT USE " + prop)
+    query = "SELECT DISTINCT ?item ?value  WHERE {?item p:" + \
+        prop + "?statement. OPTIONAL { ?item wdt:" + prop + " ?value. }}"
+    data = lookup.make_simple_wdqs_query(query, verbose=False)
+    for x in data:
+        key = lookup.sanitize_wdqs_result(x['item'])
+        value = x['value']
+        items[value] = key
+    print("FOUND {} WD ITEMS WITH PROP {}".format(len(items), prop))
+    return items
