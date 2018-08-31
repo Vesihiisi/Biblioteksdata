3c62185cb61140052d1abe23a268b2fc9b488dfc
Vesihiisi <alicia@fagerving.se>
2018-08-31 10:54:21 +0200

    start working on limit

--- importer/process_auth.py
+++ importer/process_auth.py
@@ -85,5 +85,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", required=True)
     parser.add_argument("--upload", action='store')
+    parser.add_argument("--limit",
+                        nargs='?',
+                        type=int,
+                        action='store')
     args = parser.parse_args()
     main(vars(args))
