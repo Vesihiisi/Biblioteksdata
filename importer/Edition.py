1e19f75c458d77ebeaeaeb90ceb0e6574ad85298
Vesihiisi <alicia@fagerving.se>
2018-10-16 09:45:45 +0200

    add no of pages

--- importer/Edition.py
+++ importer/Edition.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 """An object representing a Libris edition item."""
+import re
+
 from WikidataItem import WikidataItem
-# import importer_utils as utils
+import importer_utils as utils
 
 
 class Edition(WikidataItem):
@@ -25,6 +27,20 @@ class Edition(WikidataItem):
         uri = self.raw_data[0]["@id"].split("/")[-1]
         self.add_statement("libris_uri", uri)
 
+    def set_pages(self):
+        """Set number of pages."""
+        extent = self.raw_data[1].get("extent")
+        if len(extent) != 1:
+            return
+        if extent[0].get("label"):
+            extent_labels = extent[0]["label"]
+            if len(extent_labels) != 1:
+                return
+            number_strings = re.findall(r"\d+", extent_labels[0])
+            if len(number_strings) == 1:
+                no_pages = utils.package_quantity(number_strings[0])
+                self.add_statement("pages", no_pages)
+
     def __init__(self, raw_data, repository, data_files, existing, cache):
         """Initialize an empty object."""
         WikidataItem.__init__(self,
@@ -39,3 +55,4 @@ class Edition(WikidataItem):
         self.match_wikidata()
         self.set_uri()
         self.set_is()
+        self.set_pages()
